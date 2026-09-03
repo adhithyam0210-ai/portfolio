f5e5462e7c3bc0814d331bd3a9562c9659b227d1
adhithyam0210-ai <adhithyam0210@gmail.com>
2026-09-03 23:03:07 +0530

    Sync config.js with portfolio data

--- js/config.js
+++ js/config.js
@@ -74,7 +74,8 @@ const PORTFOLIO_DATA = {
         "RESTful APIs",
         "GraphQL",
         "PostgreSQL",
-        "Redis Caching"
+        "Redis Caching",
+        "java"
       ]
     },
     "tools": {
